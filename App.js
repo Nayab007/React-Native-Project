29698c488c6a125d0fd410f199101eeaf6ff8d96
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2021-12-14 02:16:25 +0100

    add

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+
+import { StyleSheet, Text, View, Button, TextInput, } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('change name'); 
+ 
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('nayab')} />
-      <StatusBar style="auto" />
+    <View >
+      <View>
+          <TextInput/>
+          <Button/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
