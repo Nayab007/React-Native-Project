38f95b19ff8a958bf4dbd0743ccc34fa197cab22
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2021-12-15 02:38:23 +0100

    add

--- App.js
+++ App.js
@@ -6,10 +6,8 @@ export default function App() {
  
   return (
     <View style={{padding: 40}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-          <TextInput placeholder="Today's Goal" style={{borderBottomColor: 'black', borderBottomWidth: 2, padding: 10}}/>
-          <Button title="ADD"/>
-      </View>
+      
+     
     
 
       <View style ={{
