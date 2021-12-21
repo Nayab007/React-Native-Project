766d9b34abeffdf8def143c54072190dd3501186
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2021-12-21 02:11:48 +0100

    add

--- App.js
+++ App.js
@@ -1,39 +1,35 @@
 import React from 'react';
 
-import { StyleSheet, Text, View, Button, TextInput, } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
- 
   return (
     <View style={styles.screen}>
-       <View style ={styles.inputContainer}>
-        
-     <TextInput
-       placeholder="Course Goal"
-       style={{
-         width: '80%',
-         borderColor: 'black',
-         borderWidth: 1,
-         padding: 10
-       }}
-      />
-    
-      <Button title="ADD" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            width: '80%',
+            borderColor: 'black',
+            borderWidth: 1,
+            padding: 10,
+          }}
+        />
+
+        <Button title="ADD" />
+      </View>
+      <View />
     </View>
-    <View/>
-    </View>
-  
   );
 }
 
 const styles = StyleSheet.create({
-    screen : {
-      padding: 50
-    },
-    inputContainer: {
-       flexDirection: 'row',
-       justifyContent: 'space-between',
-       alignItems: 'center'
-    }
-
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
 });
