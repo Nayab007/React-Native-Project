8f5243022a47544049ae4c39309bd5f39abf473b
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2021-12-29 21:22:44 +0100

    12

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     padding: 50,
   },
   inputContainer: {
-    flexDirection: 'roow',
+    flexDirection: 'rooow',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
