53c8e39a1b09811d131fa62ee4b504f0f6f31586
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2021-12-31 01:34:59 +0100

    add

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default function App() {
         <Button title="ADD" onPress={addGoalHandler}/>
       </View>
       <View>
-          {courseGoals.map((goal) => <Text>{goal}</Text>)}
+          {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
