bd0a65ea290db4531d608173f7c7096fd7648540
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-01-03 12:22:47 +0100

    add

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { View } from 'react-native-web';
 import { View,Text, StyleSheet} from 'react-native-web'
 
-const GoalItem = () => {
-    return 
+const GoalItem = props => {
+    return (
         <View style={styles.listItem} >
-        <Text>{itemData.item.value}</Text>
-    </View>;
-    
+        <Text>{props.title}</Text>
+    </View>
+    );  
 };
 
 const styles = StylesSheet.create({
