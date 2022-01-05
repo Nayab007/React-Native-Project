055de2037dff739ca1357850cf833daa858afbb0
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-01-05 03:30:22 +0100

    add

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import { View,Text, StyleSheet, TouchableOpacity} from 'react-native'
+import { BoolifiedDeprecatedTextStylePropTypes } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 const GoalItem = props => {
     return (
-        <TouchableOpacity>
+        <TouchableOpacity onPress={props.onDelete}>
         <View style={styles.listItem} >
         <Text>{props.title}</Text>
     </View>
