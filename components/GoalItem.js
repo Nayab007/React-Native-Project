b7beadcd68a3a9f3f83e33fe6b2b7e09f0b1943b
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-01-07 02:48:11 +0100

    12

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
     listItem : {
         padding: 11,
         marginVertical: 11,
-        backgroundColor: '#fccc80',
+        backgroundColor: '#ffccc80',
         borderColor: '#000000',
         borderWidth: 1
       }
