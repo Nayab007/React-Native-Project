66105d1db9fcf34e3b016d62567ba771e946e4a7
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-01-09 21:32:21 +0100

    add

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -5,7 +5,7 @@ import { View,Text, StyleSheet, TouchableOpacity} from 'react-native'
 
 const GoalItem = props => {
     return (
-        <TouchableOpacity  onPress={props.onDelete}>
+        <TouchableOpacity  onPress={props.onDelete.bind(this, props.id)}>
         <View style={styles.listItem} >
         <Text>{props.title}</Text>
     </View>
