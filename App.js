63ea2af4aba4936970a0060384f1557a2272d622
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-01-11 19:20:53 +0100

    12

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default function App() {
   };
 
   const removeGoalHandler = goalId => {
-    console.log(goalId);
+    console.log('TO BE DELETED' + goalId);
     setCourseGoals(currentGoals => {
       return currentGoals.filter((goal) => goal.id !== goalId);
     });
