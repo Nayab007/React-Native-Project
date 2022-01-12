70e8ff3319045b85beb6dbd652c5af1733ac17f7
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-01-12 07:07:08 +0100

    add

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import React, { useState } from 'react';
 
-import {
-  StyleSheet,
-  View,
-  Button,
-  FlatList,
-} from 'react-native';
-
+import { StyleSheet, View, Button, FlatList } from 'react-native';
 
 import GoalInput from './components/GoalInput';
 import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
-  const [isAddMode, setIsAddMode] =useState(false);
-  console.log('RE-RENDEING COMPONENT');
-  console.log(courseGoals);
+  const [isAddMode, setIsAddMode] = useState(false);
+ 
 
-  const addGoalHandler = goalTitle => {
+  const addGoalHandler = (goalTitle) => {
     if (goalTitle.length === 0) {
       return;
     }
@@ -28,14 +21,12 @@ export default function App() {
     setIsAddMode(false);
   };
 
-  const removeGoalHandler = goalId => {
-    console.log('TO BE DELETED' + goalId);
-    
-    setCourseGoals(currentGoals => {
+  const removeGoalHandler = (goalId) => {
+   
+    setCourseGoals((currentGoals) => {
       return currentGoals.filter((goal) => goal.id !== goalId);
     });
-    
-  }
+  };
 
   const cancelGoalAdditionHandler = () => {
     setIsAddMode(false);
@@ -44,19 +35,21 @@ export default function App() {
   return (
     <View style={styles.screen}>
       <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
-      <GoalInput 
-      visible={isAddMode} 
-      onAddGoal={addGoalHandler}
-      onCancel={cancelGoalAdditionHandler}
+      <GoalInput
+        visible={isAddMode}
+        onAddGoal={addGoalHandler}
+        onCancel={cancelGoalAdditionHandler}
       />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
-        renderItem={itemData => 
-        <GoalItem 
-        id={itemData.item.id} 
-        onDelete={removeGoalHandler} 
-        title={itemData.item.value}/>}
+        renderItem={(itemData) => (
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />
+        )}
       />
     </View>
   );
