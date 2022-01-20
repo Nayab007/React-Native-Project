78b8e1b6457671c68f1470af633a746fe07759e0
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-01-20 05:39:27 +0100

    add

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
     
       input: {
         width: '90%',
-        borderColor: '#209fe1',
+        borderColor: '#209fe2',
         borderWidth: 1,
         padding: 10,
         marginBottom: 10,
